646312c92ae38743062037b6d54326d240ca8731
Jahnvi Aghera <jahnvi.aghera111599@marwadiuniversity.ac.in>
2025-01-10 12:06:06 +0530

    clear

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'sign_in_widget.dart';
-import 'sign_up_widget.dart';
-import 'admin_dashboard_screen.dart';
-import 'user_dashboard_screen.dart';
+// import 'sign_in_widget.dart';
+// import 'sign_up_widget.dart';
+// import 'admin_dashboard_screen.dart';
+// import 'user_dashboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,14 +16,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Firebase Auth App',
+      title: 'EdVibe',
       initialRoute: '/',
       routes: {
-        '/': (context) => SignInWidget(),
-        '/signUpAdmin': (context) => SignUpWidget(), // For Admin sign up
-        '/signUpStudent': (context) => SignUpWidget(), // For Student sign up
-        '/adminDashboard': (context) => AdminDashboardScreen(),
-        '/userDashboard': (context) => UserDashboardScreen(),
+        // '/': (context) => SignInWidget(),
+        // '/signUpAdmin': (context) => SignUpWidget(), // For Admin sign up
+        // '/signUpStudent': (context) => SignUpWidget(), // For Student sign up
+        // '/adminDashboard': (context) => AdminDashboardScreen(),
+        // '/userDashboard': (context) => UserDashboardScreen(),
       },
     );
   }
